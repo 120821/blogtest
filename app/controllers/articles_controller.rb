fcd1eb1a653fb1117fa5d3b7c791eb93ec5a1f3c
linlin <1208215066@qq.com>
2023-05-25 14:54:43 +0800

    增加了show页面

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,36 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
   def new
+    @article = Article.new
   end
 
   def create
-    #render plain: params[:article].inspect
-    @article = Article.new(params[:article])
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
 
-    @article.save
-    redirect_to @article
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 
   def show
+    @article = Article.find(params[:id])
   end
 
   private
